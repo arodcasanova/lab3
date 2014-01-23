c811a310dc35dfb1548867e8a81923f84d1a4140
vagrant <vagrant@precise32.(none)>
2014-01-23 00:28:55 +0000

    lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,38 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Winning this lab");
+		$("#testjs").text("Great success!");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(updateProject); 
+}
+
+function projectClick(e) { 
+    console.log("Project clicked");
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
+
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else { 
+       description.remove();
+    }
+}
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   
+   $(projectID + " .project-description").text(newText);
 }
